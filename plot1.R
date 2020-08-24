7bd579c0f9f621d896a69d5cd102c2d60b6430f6
Aryan Jain <68581865+Aryan227@users.noreply.github.com>
2020-08-24 13:15:44 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,11 +1,11 @@
 setwd("E:/Coursera (R)")
 library("data.table")
-powerDT <- data.table::fread(input = "household_power_consumption.txt"
+df1 <- data.table::fread(input = "household_power_consumption.txt"
                              , na.strings="?")
-powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
-powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
-powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+df1[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+df1[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+df1 <- df1[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
 png("plot1.png", width=480, height=480)
-hist(powerDT[, Global_active_power], main="Global Active Power", 
+hist(df1[, Global_active_power], main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 dev.off()
