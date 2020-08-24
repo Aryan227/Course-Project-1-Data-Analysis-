b8f364eb323ae1a121251e90b3e031c9798d070d
Aryan Jain <68581865+Aryan227@users.noreply.github.com>
2020-08-24 13:26:24 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,7 +1,6 @@
 setwd("E:/Coursera (R)")
 library("data.table")
-df1 <- data.table::fread(input = "household_power_consumption.txt"
-                             , na.strings="?")
+df1 <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
 df1[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 df1[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
 df1 <- df1[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
